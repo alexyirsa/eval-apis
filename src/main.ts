4ca73da5562a95ec45eff808cd32e7f81bb2a4c3
alexyirsa <v.yirsa@uniandes.edu.co>
2023-11-17 21:14:52 -0500

    Creación del proyecto para la Evaluación

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableVersioning({
+    type: VersioningType.URI,
+    prefix: 'api/v',
+    defaultVersion: '1',
+  });
   await app.listen(3000);
 }
 bootstrap();
