e0da2df01e189d6655ef423b6f71a02177642946
alexyirsa <v.yirsa@uniandes.edu.co>
2023-11-18 22:04:30 -0500

    Definición de Pruebas

--- src/ciudad/ciudad.service.spec.ts
+++ src/ciudad/ciudad.service.spec.ts
@@ -5,11 +5,13 @@ import { TypeOrmTestingConfig } from 'src/shared/testing-utils/typeorm-testing-c
 import { Repository } from 'typeorm/repository/Repository';
 import { getRepositoryToken } from '@nestjs/typeorm/dist/common/typeorm.utils';
 import { CiudadEntity } from './ciudad.entity';
+import { faker } from '@faker-js/faker';
 
 describe('CiudadService', () => {
   let service: CiudadService;
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   let repository: Repository<CiudadEntity>;
+  let ciudadList: CiudadEntity[];
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -18,12 +20,31 @@ describe('CiudadService', () => {
     }).compile();
 
     service = module.get<CiudadService>(CiudadService);
-    repository = module.get<Repository<CiudadEntity>>(
-      getRepositoryToken(CiudadEntity),
-    );
+    // eslint-disable-next-line prettier/prettier
+    repository = module.get<Repository<CiudadEntity>>(getRepositoryToken(CiudadEntity));
+    await seedDatabase();
   });
+
+  const seedDatabase = async () => {
+    repository.clear();
+    ciudadList = [];
+    for (let i = 0; i < 5; i++) {
+      const ciudad: CiudadEntity = await repository.save({
+        name: faker.company.name(),
+        country: faker.location.country().toString(),
+        people: faker.number.int(),
+      });
+      ciudadList.push(ciudad);
+    }
+  };
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('findAll should return all cities', async () => {
+    const ciudades: CiudadEntity[] = await service.findAll();
+    expect(ciudades).not.toBeNull();
+    expect(ciudades).toHaveLength(ciudadList.length);
   });
 });
